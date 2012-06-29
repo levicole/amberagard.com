308356545b2711271f12e68dd25fa69e0c0c8b65
Levi Cole Kennedy <lckennedy@gmail.com>
2012-06-28 22:24:43 -0500

    Update title and add placeholder plugin

--- js/jquery-succes.js
+++ js/jquery-succes.js
@@ -1,3 +1,24 @@
+(function($){
+	$.fn.placeholder = function() {
+		return this.each(function() {
+			var $this = $(this);
+
+			$.data($this, "placeholder.val", $this.val());
+			$this.focus(function() {
+				var placeholder = $.data($this, "placeholder.val");
+				if ($this.val() == placeholder) { $this.val("") };
+			});
+			$this.blur(function() {
+				var placeholder = $.data($this, "placeholder.val");
+				if ($this.val() == "") { $this.val(placeholder)};
+			});
+		});
+	};
+})(jQuery);
 $(document).ready(function(){
-	$("#contactForm").submit(function() {alert('Thanks!')});
+	$(".placeholder").placeholder();
+	$("#contactForm").submit(function() {
+		$("#overlay").show();
+		setTimeout('$("#overlay").fadeOut()', 2000)
+	});
 });
